45f8c0537ff61f8b137dfda194c3aed0329a10cd
Enrique Ortiz <oice6703@gmail.com>
2019-09-01 18:54:22 -0500

    Getting nulls

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,22 +7,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  Location location;
+  @override
+  void initState() {
+    super.initState();
+    location = Location();
+    print(location.getLatitude());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
+      body: Column(
+        children: <Widget>[
+          Text('No input'),
+          Text('No input'),
+        ],
       ),
     );
   }
