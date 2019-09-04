1966fe7945a2126edc10651b1222aee7451541c8
ortizen <oice67@gmail.com>
2019-09-04 16:47:34 -0500

    Before home

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const String api = 'fa8ae1fddb735bd16147a55cfde9a358';
 
@@ -21,11 +22,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Text('No input'),
-          Text('No input'),
-        ],
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.limeAccent,
+          size: 100.0,
+        ),
       ),
     );
   }
@@ -38,8 +39,5 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper helper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$api');
     var weatherData = await helper.getData();
-    double temperature = weatherData['main']['temp'];
-    int condition = weatherData['weather'][0]['id'];
-    String cityName = weatherData['name'];
   }
 }
